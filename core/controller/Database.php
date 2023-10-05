8cbd73f3de5d1f03602ac95844546198a6986687
guidoDomingo <ruizbenitezguido11@gmail.com>
2023-10-05 15:52:11 -0300

    actualizado

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -5,7 +5,7 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="unclzepr_guido";$this->pass="";$this->host="localhost";$this->ddbb="unclzepr_hospedaje";
+		$this->user="unclzepr_guido";$this->pass='$guido$123';$this->host="localhost";$this->ddbb="unclzepr_hospedaje";
 	} 
 
 
